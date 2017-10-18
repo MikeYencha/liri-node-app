0c781944ff3db3ca81003a0d56140b40dfd28115
MikeYencha <jmyeniii@icloud.com>
2017-10-18 18:20:53 -0400

    the first set of files for my node.js homework including all calls and node modules that are hopefully ignored with my .gitignore file

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'wBvUmUJZUQAGCMD9dKLmjvF1q',
+  consumer_secret: 'eFh002tzHJkWSAqcSKqwNBSjXMv00d5QeBO1oOUCr1M5xHZFHg',
+  access_token_key: '763877574414000128-1vGu8HvIrxFfcUwpufPG9mnkFrtsxNb',
+  access_token_secret: 'oA5EEwOPN087V90xfuDhT4Gqqb6GZzfFmXIH5LuDbYKLy'
+}
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,16 @@
+var keys = require('twitterKeys');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: 'd9d598f3e33244f5ad0e1d08d4439bf4',
+    secret: 'b203de95bf8d47699dc1149435fcce9b'
+  });
+var omdb = require('omdb');
+var request = require('request');
+var fs = require('fs');
+
+
+// stuff will happen
+//.
+//..
+//...
+//...eventually
