47e89993c039379e1187eee754aab2b9931b45c3
MikeYencha <jmyeniii@icloud.com>
2017-10-21 10:12:16 -0400

    new broken code that is more exciting than my old broken code...VICTORY!

--- keys.js
+++ keys.js
@@ -2,6 +2,14 @@ var twitterKeys = {
   consumer_key: 'wBvUmUJZUQAGCMD9dKLmjvF1q',
   consumer_secret: 'eFh002tzHJkWSAqcSKqwNBSjXMv00d5QeBO1oOUCr1M5xHZFHg',
   access_token_key: '763877574414000128-1vGu8HvIrxFfcUwpufPG9mnkFrtsxNb',
-  access_token_secret: 'oA5EEwOPN087V90xfuDhT4Gqqb6GZzfFmXIH5LuDbYKLy'
+  access_token_secret: 'oA5EEwOPN087V90xfuDhT4Gqqb6GZzfFmXIH5LuDbYKLy',
 }
-module.exports = twitterKeys;
+var spotify = {
+  id: 'd9d598f3e33244f5ad0e1d08d4439bf4',
+  secret: 'b203de95bf8d47699dc1149435fcce9b'
+}
+
+module.exports = {
+  twitterKeys,
+  spotify
+}
--- liri.js
+++ liri.js
@@ -1,32 +1,30 @@
+// Basic global variables I'll need within the app
+
 var keys = require('./keys.js');
 var Twitter = require('twitter');
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify({
-//     id: 'd9d598f3e33244f5ad0e1d08d4439bf4',
-//     secret: 'b203de95bf8d47699dc1149435fcce9b'
-//   });
-// var omdb = require('omdb');
-// var request = require('request');
-// var fs = require('fs');
+var Spotify = require('node-spotify-api');
+var omdb = require('omdb');
+var request = require('request');
+var fs = require('fs');
+
+// I started using and 'if' then 'if' then 'if' etc and realized it was pretty stupid to write my code that way, even though it would have worked.
+
 //allows for first argument to be passed in node.js
 var arg = process.argv[2];
-// I started using and 'if' then 'if' then 'if' etc and realized it was pretty stupid to write my code that way, even though it would have worked.
 switch(arg) {
-
   case 'my-tweets':
+    console.log('myTweets();');
     myTweets();
     break;
-
-  case 'spotifiy-this-song':
+  case 'spotify-this-song':
+    console.log('spotifySong();');
     spotifySong();
     break;
-
   case 'movie-this':
-    movieThis();
+    console.log('movieThis();');
     break;
-
   case 'do-what-it-says':
-    doIt();
+    console.log('doIt();');
     break;
 
   default:
@@ -36,30 +34,61 @@ switch(arg) {
 
 //Here is where i'm trying to figure out my 'myTweets()' function
 
+  // function myTweets() {
+  //   console.log('This is where a Tweet should go')
+  // }
+
 function myTweets() {
-  var Client = new Twitter(keys.twitter);
-  var params = {q: 'MikeDoughnuts'};
-  Client.get('search/tweets', params, function (error, tweets, response) {
+
+  var client = new Twitter(keys.twitterKeys);
+  var params = {screen_name: 'MikeyDoughnuts'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      console.log(tweets);
-    } else {
-      console.log(error);
+      console.log(JSON.stringify(tweets,null,2));
+      
     }
-  })
+  });
+  // var params = {
+  //   screen_name: 'MikeyDoughnuts'
+  // };
+  // client.get('search/tweets', {
+  //   q: 'MikeyDoughnuts'
+  // }, function (error, tweets, response) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log(tweets);
+  //   }
+  // })
 }
 
 
 //Here is where my 'spotifySong()' will be defined
 
-// function spotifySong(song) {
-//   song = process.argv[3];
-//   spotify.search({ type: 'track', query: strong }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     } else {
-
-  
-//   console.log(data); 
-//   }
-// })
+// function spotifySong() {
+//   console.log('This is where song information should go')
 // }
+
+function spotifySong() {
+    var spotClient = new Spotify(keys.spotify);
+    var song = process.argv[3];
+    spotClient.search({ type: 'track', query: song }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      } else {
+    console.log(JSON.stringify(data,null,2)); 
+    }
+  })
+}
+
+//Here is where my 'movieThis()' will be defined
+
+function movieThis() {
+  console.log('This is where the Movie Information will go');
+}
+
+//Here is where my 'doIt()' will be defined
+
+function doIt() {
+  console.log('This is where node will read a txt file and follow the instructions');
+}
