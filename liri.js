a7bd7326b8f53ecb5f953c46cec6a234d711e968
MikeYencha <jmyeniii@icloud.com>
2017-10-20 23:29:39 -0400

    local

--- liri.js
+++ liri.js
@@ -8,10 +8,8 @@ var spotify = new Spotify({
 var omdb = require('omdb');
 var request = require('request');
 var fs = require('fs');
-
 //allows for first argument to be passed in node.js
 var arg = process.argv[2];
-
 // I started using and 'if' then 'if' then 'if' etc and realized it was pretty stupid to write my code that way, even though it would have worked.
 switch(arg) {
 
@@ -38,14 +36,30 @@ switch(arg) {
 
 //Here is where i'm trying to figure out my 'myTweets()' function
 
-function myTweets() {
-  var client = keys.twitterKeys;
-  var params = {screen_name: 'MikeyDoughnuts'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
+// function myTweets() {
+//   var Client = new Twitter(keys.twitter);
+//   var params = {q: 'MikeDoughnuts'};
+//   Client.get('search/tweets', params, function (error, tweets, response) {
+//     if (!error) {
+//       console.log(tweets);
+//     } else {
+//       console.log(error);
+//     }
+//   })
+// }
+
+
+//Here is where my 'spotifySong()' will be defined
+
+function spotifySong(song) {
+  song = process.argv[3];
+  spotify.search({ type: 'track', query: strong }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     } else {
-      console.log(error);
-    }
-  })
+
+  
+  console.log(data); 
+  }
+})
 }
